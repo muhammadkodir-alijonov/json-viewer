ff6b9b84460760ee29106b546b658bc253b77b49
muhammadqodir.a <muhammadqodir.alijonov@fido.uz>
2026-05-16 09:09:19 +0500

    rm and add

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone', // Docker uchun minimal bundle
+  allowedDevOrigins: ['10.50.71.116'], // ofis tarmog'idagi boshqa qurilmalar
 };
 
 module.exports = nextConfig;
